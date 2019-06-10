4513eff0f3ee5506173a566729bd580f80ca2418
wathigo <wathigosimon@gmail.com>
2019-06-10 07:43:10 -0400

    correted some linter errors

--- main.rb
+++ main.rb
@@ -1,45 +1,24 @@
 def bubble_sort(arr)
-    while true
-        swap = false
-        i = 0
-        while i < arr.length - 1
-            if arr[i] > arr[i+1]
-                swap = true
-                swap_var = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1] = swap_var
-            end
-            i += 1
-        end
-        if swap == false
-            break
-        end
-    end
-    arr
+  loop do
+      swap = false; i = 0
+      while i < arr.length - 1
+          (swap = true; swap_var = arr[i]; arr[i] = arr[i + 1]; arr[i + 1] = swap_var) if arr[i] > arr[i + 1]; i += 1
+      end
+      break if swap == false
+  end
+  arr
 end
 
 def bubble_sort_by(arr)
-    while true
-      swapped = false
-      index = 0
-      while index < arr.length - 1 do
-        first = arr[index]
-        second = arr[index + 1]
-        swap = yield(first, second)
-        puts "swap value: #{swap}"
-        if swap > 0
-          swap_var = arr[index]
-          arr[index] = arr[index + 1]
-          arr[index + 1] = swap_var
-          swapped = true
-        end
-        index += 1
-      end
-      if swapped == false
-        break
-      end
+  loop do
+    swapped = false; index = 0
+    while index < arr.length - 1 do
+      first = arr[index]; second = arr[index + 1]; swap = yield(first, second)
+      (swap_var = arr[index]; arr[index] = arr[index + 1]; arr[index + 1] = swap_var; swapped = true) if swap > 0; index += 1
     end
-    arr
+    break if swapped == false
+  end
+  arr
 end
 
 
